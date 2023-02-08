8a551ffba7547e52242674c6c6a854fc0a6ea182
Alexandra <achernoglazova@diasoft.ru>
2023-02-08 13:06:52 +0300

    добавила навбар

--- hiking-menu-app/src/app/app.module.ts
+++ hiking-menu-app/src/app/app.module.ts
@@ -4,15 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { WelcomePageComponent } from './components/welcome-page/welcome-page.component';
+import { ButtonModule } from 'primeng/button';
+import { ToolbarModule } from 'primeng/toolbar';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomePageComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ButtonModule,
+    ToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
